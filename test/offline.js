f09a44088bee0b05da36e0a8d138cda3f7dffc5c
constructVOD <constructvod@gmail.com>
2020-01-11 14:54:15 +0100

    1

---
+++ test/offline.js
@@ -0,0 +1,18 @@
+﻿{
+	"version": 1578744862,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/sprite-sheet0.png",
+		"images/tilemap.png",
+		"images/sprite2-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
