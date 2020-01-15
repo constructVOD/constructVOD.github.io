104a4cdf61296c339edc2f1afdc3ebec37838674
constructVOD <constructvod@gmail.com>
2020-01-15 09:22:31 +0100

    2

---
+++ ADVHPBAR/offline.js
@@ -0,0 +1,21 @@
+﻿{
+	"version": 1579076534,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/hpbar-sheet0.png",
+		"images/hpbar2-sheet0.png",
+		"media/211634__qubodup__damage.m4a",
+		"media/211634__qubodup__damage.ogg",
+		"media/41529__jamius__potiondrinklong.m4a",
+		"media/41529__jamius__potiondrinklong.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
