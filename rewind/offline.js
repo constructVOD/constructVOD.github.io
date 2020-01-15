a5ed0d8e48a009224854c9b864ff2f6b201a5876
constructVOD <constructvod@gmail.com>
2020-01-15 17:26:54 +0100

    v

---
+++ rewind/offline.js
@@ -0,0 +1,19 @@
+﻿{
+	"version": 1579105597,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/player-sheet0.png",
+		"images/player-sheet1.png",
+		"images/player-sheet2.png",
+		"images/wall-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
