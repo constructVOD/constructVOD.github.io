9f1e950c2a1319497f1ee29e500c0e1b22e170c1
constructVOD <constructvod@gmail.com>
2020-01-16 13:18:03 +0100

    df

---
+++ Puzzle/offline.js
@@ -0,0 +1,18 @@
+﻿{
+	"version": 1579177043,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/puzzleback-sheet0.png",
+		"images/tile-sheet0.png",
+		"images/tile-sheet1.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
