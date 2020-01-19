cb66521623761830c20c3f9519bc1ab478bc1e8c
constructVOD <constructvod@gmail.com>
2020-01-20 00:03:15 +0100

    d

---
+++ wybuchfiz2/offline.js
@@ -0,0 +1,19 @@
+﻿{
+	"version": 1579474922,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/solid-sheet0.png",
+		"images/box-sheet0.png",
+		"images/sprite-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
